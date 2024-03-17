7fc1c14519ae7f8c3d23b4772242bbd6d4bffd45
XeenAle <148066859+XeenAle@users.noreply.github.com>
2024-03-17 18:46:53 +0100

    feat(MySQLclass.py): add method for read file

--- src/lib/MySQLclass.py
+++ src/lib/MySQLclass.py
@@ -9,3 +9,13 @@ class MySQLDatabase:
     def __init__(self, config_file_path=config_file_path):
         self.config_file_path = config_file_path
         self.connection = None
+
+    def read_config(self):
+        try:
+            # Importa il modulo come un modulo Python
+            config = {}
+            with open(self.config_file_path, 'r') as file:
+                exec(file.read(), config)
+            return config
+        except FileNotFoundError:
+            raise Exception("File di configurazione non trovato.")
