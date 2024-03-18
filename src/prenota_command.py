9fd03639af0a9b60d56ce8db3c19ebd74760a66a
Antonino Cristaldi <uni389908@studium.unict.it>
2024-03-18 21:49:31 +0100

    feat(prenota_command.py): inizializzazione gestione principale della prenotazione

---
+++ src/prenota_command.py
@@ -0,0 +1,30 @@
+from src.lib.lib import *
+
+
+
+# Dizionario di traduzione dei giorni della settimana in italiano
+italian_day = {
+    'Monday': 'Lunedì',
+    'Tuesday': 'Martedì',
+    'Wednesday': 'Mercoledì',
+    'Thursday': 'Giovedì',
+    'Friday': 'Venerdì',
+    'Saturday': 'Sabato',
+    'Sunday': 'Domenica'
+}
+
+# Dizionario di traduzione dei mesi in italiano
+italian_month = {
+    'January': 'Gennaio',
+    'February': 'Febbraio',
+    'March': 'Marzo',
+    'April': 'Aprile',
+    'May': 'Maggio',
+    'June': 'Giugno',
+    'July': 'Luglio',
+    'August': 'Agosto',
+    'September': 'Settembre',
+    'October': 'Ottobre',
+    'November': 'Novembre',
+    'December': 'Dicembre'
+}
